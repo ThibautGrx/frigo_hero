23db1bb65f8f7e090ecb6963e1955a9d097d3cd8
Thibaut <tgorioux@ekylibre.com>
2023-10-04 17:17:04 +0200

    Add config for sending email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,15 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    port: 587,
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: "plain",
+    enable_starttls_auto: true
+  }
+
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
